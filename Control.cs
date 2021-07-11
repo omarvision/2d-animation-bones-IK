420b5ca1f005d6c99e887c9118246832bd7eed83
Omar Santiago <omarkenosia@gmail.com>
2021-07-11 12:19:46 -0400

    Add files via upload

---
+++ Control.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public float Movespeed = 1.0f;
+    private Animator anim = null;
+
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        float horz = Input.GetAxis("Horizontal");
+        anim.SetFloat("horz", horz);
+        this.transform.Translate(Vector3.right * horz * Movespeed * Time.deltaTime);
+    }
+}
